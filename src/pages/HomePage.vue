307a33cd8e619fbd66ef4254c52c37ca45f13a5a
“mafatofu” <qwqw0310@gmail.com>
2025-01-20 15:46:31 +0900

    infra : 테스트용 코드 작성

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
     <h1>Home Page</h1>
+    <input v-model="message" placeholder="Enter message" />
+    <button @click="sendMessage">Send</button>
+    <p>Response: {{ response }}</p>
     <button @click="$router.push('/next')">Go to Next Page</button>
   </div>
 </template>
+
+<script>
+import api from '../api';
+
+export default {
+  data() {
+    return {
+      message: '',
+      response: ''
+    };
+  },
+  methods: {
+    async sendMessage() {
+      try {
+        const res = await api.post('/echo', this.message);
+        this.response = res.data;
+      } catch (error) {
+        console.error("There was an error:", error);
+      }
+    }
+  }
+};
+</script>
