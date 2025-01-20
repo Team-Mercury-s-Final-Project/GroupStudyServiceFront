090d4e0ea39fd17f71fc643c6e0e9864456299a4
Laon0125 <87473656+Laon0125@users.noreply.github.com>
2025-01-20 12:10:47 +0900

    router test 완료

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
+import router from "./router"; // 라우터 임포트
 // add this
 import "./index.css";
 import "../node_modules/flowbite-vue/dist/index.css";
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(router); // Vue Router 사용 설정
+app.mount("#app");
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <button @click="$router.push('/next')">Go to Next Page</button>
+  </div>
+</template>
