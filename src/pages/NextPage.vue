2662aaa04be035ad364196174ac04090886ce042
Laon0125 <87473656+Laon0125@users.noreply.github.com>
2025-01-21 16:11:02 +0900

    백엔드에서 데이터 받아서 그룹페이지에서 표시

--- src/pages/NextPage.vue
+++ src/pages/NextPage.vue
@@ -3,14 +3,23 @@
     <h1>Next Page</h1>
     <button @click="$router.push('/')">Go Back to Home</button>
     <br />
-
-    <fwb-button
-      gradient="purple-pink"
-      @click="$router.push('/groups/{groupId}')"
-      >groupDetailPage</fwb-button
+    <FwbButton gradient="purple-pink" @click="navigateToDetailPage(1)"
+      >Go to Group 1</FwbButton
+    >
+    <FwbButton gradient="purple-pink" @click="navigateToDetailPage(2)"
+      >Go to Group 2</FwbButton
     >
   </div>
 </template>
 <script setup>
 import { FwbButton } from "flowbite-vue";
+import { useRouter } from "vue-router";
+
+// Vue Router 사용
+const router = useRouter();
+
+// 페이지 이동 함수
+function navigateToDetailPage(groupId) {
+  router.push(`/groups/${groupId}`); // groupId를 동적으로 경로에 삽입
+}
 </script>
