ab699ea6f481b3b93b0fc5a3dcb26ac5fbf37c9e
wjdwndudghlw <wjdwndudghlw@naver.com>
2025-01-30 15:33:59 +0900

    api 재 설정

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   // headers: {
   //     'Content-Type': 'application/json',
   // }
-  baseURL: "http://localhost:8080/",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
