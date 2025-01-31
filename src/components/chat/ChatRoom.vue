d6ed56b0af149fc7736c91fd4ba1e06159dd13ec
“mafatofu” <qwqw0310@gmail.com>
2025-01-31 22:11:12 +0900

    fix : 1:1채팅 닉네임 뜨지 않는 오류 수정

--- src/components/chat/ChatRoom.vue
+++ src/components/chat/ChatRoom.vue
@@ -9,28 +9,13 @@
               v-for="(message, index) in messages"
               :key="index"
               :class="{
-                'my-message': message.senderId === currentUserId,
-                'other-message': message.senderId !== currentUserId,
+                'my-message': message.senderId === this.currentUserId,
+                'other-message': message.senderId !== this.currentUserId,
               }"
             >
               <div class="message-content">
                 <strong>{{ message.nickName }}</strong
                 >: {{ message.content }}
-              </div>
-              <!-- 첨부 파일 표시 -->
-              <div v-if="message.messageFiles.length > 0" class="message-files">
-                <div
-                  v-for="(file, index) in message.messageFiles"
-                  :key="index"
-                  class="message-file"
-                >
-                  <div v-if="file.fileType && file.fileType.includes('image')">
-                    <img :src="file.fileUrl" alt="첨부 이미지" />
-                  </div>
-                  <div v-else>
-                    <a :href="file.fileUrl" target="_blank">파일 다운로드</a>
-                  </div>
-                </div>
               </div>
               <div class="message-timestamp">{{ message.createdAt }}</div>
             </div>
@@ -49,126 +34,113 @@
     </div>
   </div>
 </template>
-
 <script>
-import api from "../../api.js";
 import Stomp from "stompjs";
-import * as jwtDecode from "jwt-decode"; // 수정된 import 문
+import * as jwtDecode from "jwt-decode";
+import axiosInstance from "../../api/axiosInstance_test.js";
 
+let token = "";
 export default {
   data() {
     return {
-      messages: [], // 메시지 배열
+      messages: [],
       newMessage: "",
-      chatRoomId: -999, // 채팅방 ID
-      stompClient: null, // stompClient 초기화
-      currentUserId: null, // 현재 사용자 ID 초기화
-      receiverId: null, // 수신자 ID 초기화
-      isDataLoaded: false, // 데이터 로드 상태
+      chatRoomId: -999,
+      stompClient: null,
+      currentUserId: null,
+      receiverId: null,
+      isDataLoaded: false,
       nickName: "",
+      chatRoomType: "DM",
     };
   },
-  mounted() {
-    this.chatRoomId = this.$route.params.chatRoomId; // URL에서 chatRoomId 가져오기
 
-    // JWT 토큰에서 사용자 정보 추출
-    const token = localStorage.getItem("access");
+  mounted() {
+    token = localStorage.getItem("access");
+    this.chatRoomId = this.$route.params.chatRoomId;
+
     if (token) {
-      const decodedToken = jwtDecode.jwtDecode(token); // 수정된 호출 방식
-      this.currentUserId = decodedToken.id; // jwt 토큰에서 userId 추출
+      const decodedToken = jwtDecode.jwtDecode(token);
+      this.currentUserId = decodedToken.id;
     }
 
-    this.loadChatHistory(); // 채팅 내역 불러오기
-
-    // WebSocket 연결 설정
-    let socket = new WebSocket("ws://localhost:8080/chat");
-    this.stompClient = Stomp.over(socket);
-
-    this.stompClient.connect(
-      {},
-      (frame) => {
-        console.log("Connected: " + frame);
-        // 특정 경로를 구독
-        this.stompClient.subscribe(
-          `/topic/chat.${this.chatRoomId}`,
-          (messageOutput) => {
-            try {
-              const message = JSON.parse(messageOutput.body);
-
-              // 수신된 메시지 데이터를 콘솔에 출력하여 구조 확인
-              console.log("수신된 메시지 데이터:", message);
-
-              // 메시지 데이터를 messages 배열에 추가
-              this.messages.push({
-                id: message.data.id,
-                senderId: message.data.senderId, // Sender ID 설정
-                nickName: message.data.nickName, // Nickname 설정. 상대방의 닉네임
-                content: message.data.messageContent,
-                createdAt: new Date(message.data.createdAt).toLocaleString(),
-                unreadCount: message.data.unreadCount,
-                messageFiles: message.data.messageFiles || [],
-              });
-
-              console.log("추가된 메시지:", this.messages);
-            } catch (error) {
-              console.error("메시지 파싱 오류:", error);
-            }
-          }
-        );
-      },
-      (error) => {
-        console.error("WebSocket 연결 오류:", error);
-        // 재연결 시도
-        setTimeout(() => {
-          this.stompClient.connect();
-        }, 10000); // 10초 후 재연결 시도
-      }
-    );
+    this.connectWebSocket();
+    this.loadChatHistory();
   },
 
   methods: {
-    loadChatHistory() {
-      api
-        .get(`/chats/${this.chatRoomId}`, { timeout: 10000 }) // 10초 타임아웃 설정
-        .then((response) => {
-          console.log("응답 데이터:", JSON.stringify(response.data, null, 2));
+    connectWebSocket() {
+      const socket = new WebSocket(`ws://localhost:8080/chat`);
+      this.stompClient = Stomp.over(socket);
 
+      this.stompClient.connect(
+        {},
+        (frame) => {
+          console.log("Connected: " + frame);
+          this.stompClient.subscribe(
+            `/topic/chat.${this.chatRoomId}`,
+            (messageOutput) => {
+              try {
+                const message = JSON.parse(messageOutput.body);
+                console.log("Received message:", message);
+
+                this.messages.push({
+                  id: message.data.id,
+                  senderId: message.data.senderId,
+                  nickName: message.data.nickName || "알 수 없음",
+                  content: message.data.messageContent,
+                  createdAt: new Date(message.data.createdAt).toLocaleString(),
+                });
+              } catch (error) {
+                console.error("메시지 파싱 오류:", error);
+              }
+            }
+          );
+        },
+        (error) => {
+          console.error("WebSocket 연결 오류:", error);
+          setTimeout(() => {
+            this.connectWebSocket();
+          }, 10000);
+        }
+      );
+    },
+
+    loadChatHistory() {
+      axiosInstance
+        .get(`/chats/${this.chatRoomId}`, {
+          timeout: 10000,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
           if (response.data && response.data.success && response.data.data) {
             const chatData = response.data.data;
 
-            // chatMembers에서 현재 사용자의 nickName 설정
-            if (Array.isArray(chatData.chatMembers)) {
-              const currentUser = chatData.chatMembers.find(
-                (member) => member.id === this.currentUserId
-              );
-              if (currentUser) {
-                this.nickName = currentUser.nickName; // 현재 사용자의 nickName 설정
-              }
-
-              // 현재 사용자와 일치하지 않는 멤버를 찾아 receiverId 설정
-              const receiver = chatData.chatMembers.find(
-                (member) => member.id !== this.currentUserId
-              );
-              if (receiver) {
-                this.receiverId = receiver.id; // 상대방의 ID로 receiverId 설정
-              }
-            }
-
-            // messages 설정
             if (Array.isArray(chatData.messages)) {
               this.messages = chatData.messages.map((msg) => ({
                 id: msg.id,
                 senderId: msg.senderId,
-                nickName: msg.nickName,
+                nickName: msg.nickName || "알 수 없음",
                 content: msg.content,
                 createdAt: new Date(msg.createdAt).toLocaleString(),
-                unreadCount: msg.unreadCount,
-                messageFiles: msg.messageFiles || [],
               }));
+
+              // JWT ID와 같지 않은 데이터의 ID를 receiverId로 설정
+              const receiver = chatData.messages.find(
+                (msg) => msg.senderId !== this.currentUserId
+              );
+              this.receiverId = receiver ? receiver.senderId : null;
+
+              const sender = chatData.messages.find(
+                (msg) => msg.senderId === this.currentUserId
+              );
+              this.nickName = sender.nickName;
             }
 
-            console.log("설정된 메시지 배열:", this.messages);
-            this.isDataLoaded = true; // 데이터 로드 완료
+            this.isDataLoaded = true;
           } else {
             console.error("알 수 없는 데이터 구조:", response.data);
             this.messages = [];
@@ -187,34 +159,28 @@ export default {
         const payload = {
           chatRoomId: this.chatRoomId,
           senderId: this.currentUserId,
-          receiverId: this.receiverId, // 추가
+          receiverId: this.receiverId,
           nickName: this.nickName,
           messageContent: this.newMessage,
-          messageFiles: this.messageFiles || [], // 추가
+          chatRoomType: this.chatRoomType,
         };
 
-        this.stompClient.send(
-          `/pub/chat/sendTextMessage/${this.chatRoomId}`,
-          {},
-          JSON.stringify(payload)
-        );
+        if (this.stompClient && this.stompClient.connected) {
+          this.stompClient.send(
+            `/pub/chat/sendTextMessage/${this.chatRoomId}`,
+            {},
+            JSON.stringify(payload)
+          );
+        } else {
+          console.error("WebSocket is not connected.");
+        }
 
-        // 메시지를 보내고 나서 newMessage를 초기화
         this.newMessage = "";
       }
     },
   },
 };
 </script>
-
-<style scoped>
-.loading {
-  text-align: center;
-  padding: 20px;
-  font-size: 18px;
-  color: #666;
-}
-</style>
 
 <style>
 * {
@@ -248,7 +214,7 @@ body {
   color: #fff;
   padding: 2rem;
   border-radius: 10px;
-  margin-top: 3.5rem; /* 네비게이션 바 높이 조정 */
+  margin-top: 3.5rem;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -274,21 +240,12 @@ body {
 .message {
   margin-bottom: 1rem;
 }
-.message-sender {
-  font-weight: bold;
-}
 .message-content {
   margin: 0.5rem 0;
 }
 .message-timestamp {
   font-size: 0.8rem;
   color: #888;
-}
-.date-separator {
-  text-align: center;
-  margin: 1rem 0;
-  font-weight: bold;
-  color: #999;
 }
 .input-container {
   display: flex;
@@ -311,9 +268,5 @@ button {
   background-color: #f7c02b;
   color: #333;
   cursor: pointer;
-}
-.chat-list {
-  background-color: #555;
-  color: #fff;
 }
 </style>
